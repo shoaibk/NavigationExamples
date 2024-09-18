bc63615b6385197bd173149654f21ba4eb622d3b
Shoaib Khan <shb.khan@gmail.com>
2024-09-18 17:47:25 -0400

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();
+
+function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Home Screen</Text>
+      <Button
+        title="Details Screen"
+        onPress={() => navigation.navigate("Details")}
+      />
       <StatusBar style="auto" />
     </View>
+  );
+}
+
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>Details Screen</Text>
+      <Button title="Home Screen" onPress={() => navigation.navigate("Home")} />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        {/* <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Details" component={DetailsScreen} />
+        </Stack.Navigator> */}
+        {/* <Drawer.Navigator>
+          <Drawer.Screen name="Home" component={HomeScreen} />
+          <Drawer.Screen name="Details" component={DetailsScreen} />
+        </Drawer.Navigator> */}
+
+        <Tab.Navigator>
+          <Tab.Screen
+            name="Home"
+            component={HomeScreen}
+            // options={{
+            //   tabBarIcon: ({ focus, size, color }) => (
+            //     <Ionicons name={"home-outline"} size={size} color={color} />
+            //   ),
+            // }}
+          />
+          <Tab.Screen name="Details" component={DetailsScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
